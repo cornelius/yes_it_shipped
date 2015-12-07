7f405e3d06be4b3177bb8d2089a1f15178ea069c
Cornelius Schumacher <schumacher@kde.org>
2015-12-07 22:36:52 +0100

    Add missing release_date_time attribute

--- spec/views/releases/index.html.haml_spec.rb
+++ spec/views/releases/index.html.haml_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "releases/index.html.haml", type: :view do
   it "lists releases" do
-    assign(:releases, [Release.new(project: "dummy", version: "0.0.1")])
+    assign(:releases, [Release.new(project: "dummy", version: "0.0.1",
+      release_date_time: "20151207T2236")])
     render
     expect(rendered).to include "dummy"
     expect(rendered).to include "0.0.1"
