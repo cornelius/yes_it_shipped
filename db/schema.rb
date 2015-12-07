b6cdb75abefec4a147705ef21a48349929ec2b05
Cornelius Schumacher <schumacher@kde.org>
2015-12-07 23:26:11 +0100

    Switch to PostgreSQL
    Copy the file `config/database.yml.template` to `config/database.yml`
    and change the username to the user who has access to the PostgreSQL
    database.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151207152949) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "releases", force: :cascade do |t|
     t.string   "project"
     t.string   "project_url"
