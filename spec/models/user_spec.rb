3ab9b4787f8bfcc946c6d4166aab36b77946895d
Mauro Morales <contact@mauromorales.com>
2015-12-09 13:06:44 +0100

    Give admin permission just to the first user to be created

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  it "fails when empty" do
+    expect {
+      User.create!
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  describe "setting the user's role" do
+    context "if it's the first user to be created" do
+      before(:each) do
+        User.delete_all
+      end
+
+      it "gets admin rights" do
+        user = User.create!(email: "admin@example.com", password: "12345678")
+        expect(user).to be_admin
+      end
+    end
+
+    context "if it's not the first user to be created" do
+      before(:each) do
+        FactoryGirl.create(:user)
+      end
+
+      it "gets normal rights" do
+        user = User.create!(email: "not_admin@example.com", password: "12345678")
+        expect(user).not_to be_admin
+      end
+    end
+  end
+
+end
