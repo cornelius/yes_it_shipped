e7818b5daeee78c9444ef261dafd1eaf310848ed
Mauro Morales <contact@mauromorales.com>
2015-12-09 21:03:37 +0100

    Don't allow releases in the future

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,4 +1,12 @@
 class Release < ActiveRecord::Base
   validates :version, :project, :project_url, :release_date_time, :release_url,
     :ysi_config_url, presence: true
+
+  validate :release_date_time_cannot_be_in_the_future
+
+  def release_date_time_cannot_be_in_the_future
+    if release_date_time.present? && release_date_time > Time.now
+      errors.add(:release_date_time, "can't be in the future")
+    end
+  end
 end
--- spec/models/release_spec.rb
+++ spec/models/release_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Release, type: :model do
       Release.create!
     }.to raise_error(ActiveRecord::RecordInvalid)
   end
+
+  it "fails when the release date is in the future" do
+    expect {
+      Release.create!(
+        version: "0.1.0",
+        project: "Dummy",
+        project_url: "https://example.com/dummy",
+        release_date_time: Time.now + 1.days,
+        release_url: "https://example.com/dummy/v0.1.0",
+        ysi_config_url: "https://example.com/dummy/yes_ship_it.conf"
+       )
+    }.to raise_error(ActiveRecord::RecordInvalid)
+  end
 end
